50a07576ab6a087854979c265580f4aa81daebc4
TomasUribe <uribectomas@gmail.com>
2021-08-21 15:00:28 -0500

    Updated player movement
    Movement now runs left to right animated and flipping in direction :)

---
+++ PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    private bool facingRight = true;
+
+    public float speed = 2.0f;
+    public float horizMovement;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizMovement = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+        Flip(horizMovement);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+
+    }
+
+    private void Flip (float horizontal)
+    {
+        if (horizontal < 0 && facingRight || horizontal > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+    }
+}
